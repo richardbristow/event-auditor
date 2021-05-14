bc68d8f6d2c2550285df358ea606c3d1a2468a48
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-05-14 16:05:01 +0100

    remove serviceworker

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import {
 } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 const darkTheme = createMuiTheme({
   palette: {
@@ -29,8 +28,3 @@ ReactDOM.render(
   // </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
